6604490819d1ec58ae28e8a71c34801966a9903c
Tyler Mayoff <tyler@tylermayoff.com>
2023-03-24 20:04:54 -0400

    Added some more tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,18 +1,51 @@
 use assert_cmd::prelude::*;
-use std::process::Command;
+use std::process::{Command, Output};
+
+fn get_output(output: &Output) -> String {
+    String::from_utf8_lossy(&output.stdout).to_string()
+}
 
 #[test]
-fn simple_test() -> Result<(), Box<dyn std::error::Error>> {
+fn ls_test() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("ric")?;
-    cmd.args(["--image", "debian", "--", "ls", "/tmp/"]);
-
-    let output = cmd.output().expect("Failed to get output");
-    let stderr = &output.stderr;
-    let stdout = &output.stdout;
+    cmd.args(["--image", "debian", "--", "ls"]);
     cmd.assert().success();
 
-    println!("stderr: {}", String::from_utf8_lossy(stderr));
-    println!("stdout: {}", String::from_utf8_lossy(stdout));
+    let mut expected_cmd = Command::new("ls");
+    expected_cmd.assert().success();
+
+    let got_output = get_output(&cmd.output()?);
+    assert!(!got_output.is_empty());
+    let expected_output = get_output(&expected_cmd.output()?);
+    assert!(!expected_output.is_empty());
+
+    println!("Got: {}", got_output);
+    println!("Expected: {}", expected_output);
+
+    assert!(expected_output == got_output);
+
+    Ok(())
+}
+
+#[test]
+fn cat_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ric")?;
+    cmd.args(["--image", "debian", "--", "cat", ".gitignore"]);
+    cmd.assert().success();
+
+    let mut expected_cmd = Command::new("cat");
+    expected_cmd.arg(".gitignore");
+    expected_cmd.assert().success();
+
+    let got_output = get_output(&cmd.output()?);
+    assert!(!got_output.is_empty());
+    let expected_output = get_output(&expected_cmd.output()?);
+    assert!(!expected_output.is_empty());
+
+    println!("Got: {}", got_output);
+    println!("Expected: {}", expected_output);
+
+    assert!(expected_output == got_output);
 
     Ok(())
 }
