63139cc9f48a12b85d9f4e095b8f3ef0fb43e5b1
Tyler Mayoff <tyler@tylermayoff.com>
2023-03-26 12:31:58 -0400

    Switched to async-std

--- src/main.rs
+++ src/main.rs
@@ -29,51 +29,48 @@ fn setup_signal_handler(
     ctrlc::set_handler(move || {
         log::info!("Stopping container");
         let mut container_id = container_id.clone();
-        tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap()
-            .block_on(async {
-                let container = docker.containers().get(container_id.take().unwrap());
-                if let Err(e) = container.kill(None).await {
-                    log::error!("Failed to stop container {}", e)
-                }
+        async_std::task::block_on(async {
+            let container = docker.containers().get(container_id.take().unwrap());
+            if let Err(e) = container.kill(None).await {
+                log::error!("Failed to stop container {}", e)
+            }
 
-                docker::cleanup_container(&container).await;
-            });
+            docker::cleanup_container(&container).await;
+        });
     })
 }
 
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    env_logger::init();
+fn main() -> Result<(), anyhow::Error> {
+    async_std::task::block_on(async {
+        env_logger::init();
 
-    let mut args = Args::parse();
+        let mut args = Args::parse();
 
-    if args.command.is_empty() {
-        log::warn!("No command was specified, finishing early");
-        return Ok(());
-    }
+        if args.command.is_empty() {
+            log::warn!("No command was specified, finishing early");
+            return Ok(());
+        }
 
-    args.command = vec![
-        "/bin/sh".to_string(),
-        "-c".to_string(),
-        args.command.join(" "),
-    ];
+        args.command = vec![
+            "/bin/sh".to_string(),
+            "-c".to_string(),
+            args.command.join(" "),
+        ];
 
-    if args.image.is_none() && args.container.is_none() {
-        bail!("Must provide either an image or a container to use");
-    }
+        if args.image.is_none() && args.container.is_none() {
+            bail!("Must provide either an image or a container to use");
+        }
 
-    let mut docker =
-        docker_api::Docker::new("unix:///var/run/docker.sock").expect("Docker must be running");
-    docker.adjust_api_version().await?;
+        let mut docker =
+            docker_api::Docker::new("unix:///var/run/docker.sock").expect("Docker must be running");
+        docker.adjust_api_version().await?;
 
-    if let Some(image) = &args.image {
-        docker::pull_if_needed(&docker, image).await?;
-    }
+        if let Some(image) = &args.image {
+            docker::pull_if_needed(&docker, image).await?;
+        }
 
-    docker::runner(&docker, args).await?;
+        docker::runner(&docker, args).await?;
 
-    Ok(())
+        Ok(())
+    })
 }
